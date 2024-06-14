a5b76ef8d7515a4d47c4311420124cb94d818172
anpigon <markan82@gmail.com>
2024-06-14 15:13:46 +0900

    feat(agents/index.tsx): add "Add Agents" button to AgentSetting component with a handler for the button click event

--- src/features/setting/components/agents/index.tsx
+++ src/features/setting/components/agents/index.tsx
@@ -1,8 +1,20 @@
-import { SettingItem } from "@/components/settings/setting-item";
-import { useTranslation } from "react-i18next";
+import {Button} from '@/components';
+import {SettingItem} from '@/components/settings/setting-item';
+import {useTranslation} from 'react-i18next';
 
-export default function AgentSetting()  {
+export default function AgentSetting() {
 	const {t} = useTranslation('settings');
-	
-	return <><SettingItem heading name={t('Agents')} /></>
+
+	const handleAddAgent = () => {
+		console.log(1);
+	};
+
+	return (
+		<>
+			<SettingItem heading name={t('Agents')} />
+			<SettingItem name="">
+				<Button onClick={handleAddAgent}>Add Agents</Button>
+			</SettingItem>
+		</>
+	);
 }
