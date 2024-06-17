00e0c6fba7dded61056ab9cc8276d309d7c3b0b1
anpigon <markan82@gmail.com>
2024-06-17 15:51:05 +0900

    refactor: Update hashString function in utils/hash.ts to use SHA-1 algorithm for improved compatibility with existing systems

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -4,7 +4,7 @@ export async function hashString(inputString: string): Promise<string> {
 	// return crypto.createHash('sha1').update(inputString, 'utf-8').digest('hex');
 	const encoder = new TextEncoder();
 	const data = encoder.encode(inputString);
-	const hashBuffer = await window.crypto.subtle.digest('SHA-256', data); // hash the message
+	const hashBuffer = await window.crypto.subtle.digest('SHA-1', data); // hash the message
 	const hashArray = Array.from(new Uint8Array(hashBuffer)); // convert buffer to byte array
 	const hashHex = hashArray.map(byte => byte.toString(16).padStart(2, '0')).join(''); // convert bytes to hex string
 	return hashHex;
