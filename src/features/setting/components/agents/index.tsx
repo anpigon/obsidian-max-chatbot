1138c644d62855f6eeff536d4107a834b2893a07
anpigon <markan82@gmail.com>
2024-06-17 14:35:41 +0900

    feat: Organize language model imports in LanguageModels component

--- src/features/setting/components/agents/index.tsx
+++ src/features/setting/components/agents/index.tsx
@@ -1,12 +1,42 @@
 import {Button} from '@/components';
 import {SettingItem} from '@/components/settings/setting-item';
+import {SystemMessage} from '@langchain/core/messages';
+import {OllamaEmbeddings} from '@langchain/community/embeddings/ollama';
+
 import {useTranslation} from 'react-i18next';
+import {usePlugin} from '@/hooks/useApp';
+import {normalizePath} from 'obsidian';
+import {LanceDB} from '@langchain/community/vectorstores/lancedb';
+import {connect} from 'vectordb';
 
 export default function AgentSetting() {
+	const plugin = usePlugin();
 	const {t} = useTranslation('settings');
 
-	const handleAddAgent = () => {
+	const handleAddAgent = async () => {
 		console.log(1);
+
+		const embeddings = new OllamaEmbeddings({
+			model: 'nomic-embed-text', // default value
+			baseUrl: 'http://localhost:11434', // default value
+			// requestOptions: {
+			// 	useMMap: true, // use_mmap 1
+			// 	numThread: 6, // num_thread 6
+			// 	numGpu: 1, // num_gpu 1
+			// },
+		});
+
+		const dir = normalizePath(plugin.manifest.dir + '/lancedb-');
+		if (!(await plugin.app.vault.adapter.exists(dir))) {
+			await plugin.app.vault.adapter.mkdir(dir);
+		}
+		const db = await connect(dir);
+		const table = await db.createTable('vectors', []);
+		const vectorStore = await LanceDB.fromDocuments([], embeddings, {
+			table,
+		});
+
+		const resultOne = await vectorStore.similaritySearch('hello world', 1);
 	};
 
 	return (
