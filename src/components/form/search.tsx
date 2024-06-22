1cb75ad28bb8bcc43040d2a2b1a60a520d0cd572
anpigon <markan82@gmail.com>
2024-06-22 12:05:48 +0900

    feat(search.tsx): refactor Search component to use React hooks, add support for imperative handling and improve accessibility fix(search.tsx): fix type errors and add missing types for ChangeEvent and FormEvent

--- src/components/form/search.tsx
+++ src/components/form/search.tsx
@@ -1,13 +1,62 @@
-import {FC, InputHTMLAttributes} from 'react';
+import {InputHTMLAttributes, forwardRef, useEffect, useImperativeHandle, useRef, useState} from 'react';
 import {twMerge} from 'tailwind-merge';
 
-export type SearchProps = InputHTMLAttributes<HTMLInputElement>;
+import type {ChangeEvent, FormEvent} from 'react';
 
-export const Search: FC<SearchProps> = ({className, ...props}) => {
+export interface SearchProps extends InputHTMLAttributes<HTMLInputElement> {
+	// eslint-disable-next-line no-unused-vars
+	onSearch?: (element: HTMLInputElement) => void;
+}
+
+export const Search = forwardRef<HTMLInputElement, SearchProps>(({className, value, onInput, onChange, onSearch, ...props}, ref) => {
+	const internalRef = useRef<HTMLInputElement>(null);
+	const [internalValue, setInternalValue] = useState(value);
+
+	useImperativeHandle(ref, () => internalRef.current!);
+
+	useEffect(() => {
+		if (onSearch && internalRef.current) {
+			onSearch(internalRef.current);
+		}
+	}, [onSearch, internalRef.current]);
+
+	const handleClear = () => {
+		setInternalValue('');
+		if (internalRef.current) {
+			internalRef.current.value = '';
+			internalRef.current.focus();
+		}
+	};
+
+	const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+		setInternalValue(event.target.value);
+		if (onChange) {
+			onChange(event);
+		}
+	};
+
+	const handleInput = (event: FormEvent<HTMLInputElement>) => {
+		setInternalValue(event.currentTarget.value);
+		if (onInput) {
+			onInput(event);
+		}
+	};
+
 	return (
 		<div className={twMerge('search-input-container', className)}>
-			<input type="search" spellCheck={false} enterKeyHint="search" {...props} />
-			<div className="search-input-clear-button" />
+			<input
+				type="search"
+				spellCheck={false}
+				enterKeyHint="search"
+				value={internalValue}
+				onChange={handleChange}
+				onInput={handleInput}
+				{...props}
+				ref={internalRef}
+			/>
+			<div role="button" aria-label="reset" className="search-input-clear-button" onClick={handleClear} />
 		</div>
 	);
-};
+});
+
+Search.displayName = 'Search';
