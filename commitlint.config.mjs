dd54042bb5c2b23258d4e40ae6471b40de79807e
anpigon <markan82@gmail.com>
2024-06-25 13:30:36 +0900

    chore: Update commitlint.config.mjs to enforce subject case rules

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -2,6 +2,8 @@ export default {
 	extends: ['@commitlint/config-conventional'],
 	rules: {
 		'header-max-length': [2, 'always', 200],
+		'body-max-line-length': [2, 'never', 2000],
 		'subject-case': [2, 'always', ['lower-case', 'upper-case', 'camel-case', 'kebab-case', 'pascal-case', 'sentence-case', 'snake-case', 'start-case']],
+		'body-leading-blank': [2, 'never'],
 	},
 };
