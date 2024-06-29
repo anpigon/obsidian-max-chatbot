c45f51d56229ff78ea77700117d6d780f09f703c
anpigon <markan82@gmail.com>
2024-06-29 12:23:14 +0900

    feat(chatbot): add chat history drawer and new session handling refactor(chatbot-header): use leftComponent and rightComponent props for buttons fix(chatbot): import useState in chatbot.tsx and remove redundant imports

--- src/features/chatbot/chatbot.tsx
+++ src/features/chatbot/chatbot.tsx
@@ -1,5 +1,5 @@
+import {useEffect, useRef, useState} from 'react';
 import {useTranslation} from 'react-i18next';
-import {useEffect, useRef} from 'react';
 import {Notice} from 'obsidian';
 
 import type {ChangeEvent, FC, KeyboardEvent} from 'react';
@@ -12,12 +12,15 @@ import useOnceEffect from '@/hooks/useOnceEffect';
 import {MessagesContainer} from './components/messages-container';
 import {ChatbotContainer} from './components/chatbot-container';
 import {useScrollToBottom} from './hooks/use-scroll-to-bottom';
+import {ChatHistories} from './components/chat-histories';
 import {ChatbotHeader} from './components/chatbot-header';
 import {useCurrentModel} from './hooks/use-current-model';
 import {ChatBox} from './components/chat-box';
 import {Message} from './components/message';
+import {Drawer} from './components/drawer';
 import {LLM_PROVIDERS} from '@/constants';
 import {useChatbotState} from './context';
+import {IconButton} from '@/components';
 import {useLLM} from './hooks/use-llm';
 
 export const Chatbot: FC = () => {
@@ -29,6 +32,14 @@ export const Chatbot: FC = () => {
 	const chatBoxRef = useRef<HTMLTextAreaElement>(null);
 	const messageContainerRef = useRef<HTMLDivElement>(null);
 	const messageEndRef = useRef<HTMLDivElement>(null);
+
+	const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+	const handleViewHistory = () => {
+		setIsDrawerOpen(true);
+	};
+	const handleCloseDrawer = () => {
+		setIsDrawerOpen(false);
+	};
 
 	const chatbotName = settings?.appearance?.chatbotName ?? DEFAULT_SETTINGS.appearance.chatbotName;
 	const username = settings?.appearance.userName || DEFAULT_SETTINGS.appearance.userName;
@@ -42,13 +53,19 @@ export const Chatbot: FC = () => {
 
 	const scrollToBottom = useScrollToBottom(messageContainerRef);
 
-	const {messages, setMessages, isStreaming, controller, setMessage, processMessage, deleteMessage, updateMessage} = useLLM({
+	const {newSession, sessionID, loadChatHistory, messages, isStreaming, controller, setMessage, processMessage, deleteMessage, updateMessage} = useLLM({
 		provider: currentModel.provider,
 		model: currentModel.model,
 		systemPrompt: defaultSystemPrompt,
 		allowReferenceCurrentNote,
 		handlers: {onMessageAdded: scrollToBottom},
 	});
+
+	useEffect(() => {
+		if (!sessionID || !messages.length) return;
+		// Save chat history to the file
+		plugin.saveChatHistory(sessionID, messages);
+	}, [sessionID, messages]);
 
 	useOnceEffect(() => {
 		chatBoxRef.current?.focus();
@@ -113,8 +130,7 @@ export const Chatbot: FC = () => {
 
 	const handleStartNewChat = () => {
 		controller?.abort();
-		setMessages([]);
-		setMessage('');
+		newSession();
 		resetInputForm();
 		scrollToBottom();
 	};
@@ -127,8 +143,16 @@ export const Chatbot: FC = () => {
 				currentModel={currentModel}
 				disabled={isStreaming}
 				onChangeModel={handleChangeModel}
-				onStartNewChat={handleStartNewChat}
+				leftComponent={<IconButton label={t('chat history')} icon="history" onClick={handleViewHistory} />}
+				rightComponent={<IconButton className="absolute top-2 right-2" label={t('Start new chat')} icon="plus" onClick={handleStartNewChat} />}
 			/>
+			<Drawer side="left" isOpen={isDrawerOpen} onClose={handleCloseDrawer}>
+				<div className="p-4 relative">
+					<h3 className="text-lg font-semibold p-0 mt-0 mb-4">{t('Chat History')}</h3>
+					<IconButton className="absolute top-4 right-4" label={t('close', {ns: 'common'})} icon="x" onClick={handleCloseDrawer} />
+					{isDrawerOpen && <ChatHistories />}
+				</div>
+			</Drawer>
 
 			<MessagesContainer ref={messageContainerRef}>
 				{messages.map(({role, content, id, showLoading}, i) =>
--- src/features/chatbot/components/chatbot-header.tsx
+++ src/features/chatbot/components/chatbot-header.tsx
@@ -1,12 +1,8 @@
-import {useState, type ChangeEventHandler, type FC, type PropsWithChildren} from 'react';
+import {ReactNode, type ChangeEventHandler, type FC, type PropsWithChildren} from 'react';
 
-import {useTranslation} from 'react-i18next';
-
-import {IconButton} from '@/components/buttons/icon-button';
 import {ProviderModels} from '@/hooks/useEnabledModels';
 import {Dropdown} from '@/components/form/dropdown';
 import {LLM_PROVIDERS} from '@/constants';
-import {Drawer} from './drawer';
 
 interface ChatbotHeaderProps extends PropsWithChildren {
 	botName: string;
@@ -18,12 +14,11 @@ interface ChatbotHeaderProps extends PropsWithChildren {
 	};
 	// eslint-disable-next-line no-unused-vars
 	onChangeModel: (provider: LLM_PROVIDERS, modelName: string) => void;
-	onStartNewChat: () => void;
+	leftComponent?: ReactNode;
+	rightComponent?: ReactNode;
 }
 
-export const ChatbotHeader: FC<ChatbotHeaderProps> = ({botName, providers, disabled, currentModel, onChangeModel, onStartNewChat}) => {
-	const {t} = useTranslation('chatbot');
-
+export const ChatbotHeader: FC<ChatbotHeaderProps> = ({botName, providers, disabled, currentModel, onChangeModel, leftComponent, rightComponent, children}) => {
 	const handleChangeModel: ChangeEventHandler<HTMLSelectElement> = e => {
 		const value = e.target.value;
 		if (value) {
@@ -32,51 +27,38 @@ export const ChatbotHeader: FC<ChatbotHeaderProps> = ({botName, providers, disab
 		}
 	};
 
-	const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-	const handleViewHistory = () => {
-		setIsDrawerOpen(true);
-	};
-	const handleCloseDrawer = () => {
-		setIsDrawerOpen(false);
-	};
-
 	return (
-		<>
-			<div className="py-4 px-0 text-center relative">
-				<h2 className="mt-0 mb-0 p-0 text-xl w-full text-center">{botName}</h2>
-				<Dropdown
-					className="h-fit px-0 py-1 shadow-none hover:shadow-none text-center text-xs mr-1 pr-4"
-					value={`${currentModel.provider}/${currentModel.model}`}
-					onChange={handleChangeModel}
-					disabled={disabled}
-				>
-					{providers
-						?.filter(({provider, models}) => models.length > 0)
-						.map(({provider, models}) => {
-							return (
-								<optgroup key={provider} label={provider}>
-									{models.map(model => {
-										const value = `${provider}/${model}`;
-										return (
-											<option key={value} value={value}>
-												{model}
-											</option>
-										);
-									})}
-								</optgroup>
-							);
-						})}
-				</Dropdown>
-				<IconButton className="absolute top-2 right-2" label={t('Start new chat')} icon="plus" onClick={onStartNewChat} />
-				<IconButton className="absolute top-2 left-2" label={t('view history')} icon="history" onClick={handleViewHistory} />
-				<Drawer isOpen={isDrawerOpen} onClose={handleCloseDrawer} side="left">
-					<div className="p-4 relative">
-						<h3 className="text-lg font-semibold mb-4">{t('Chat History')}</h3>
-						<IconButton className="absolute top-4 right-4" label={t('close', {ns: 'common'})} icon="x" onClick={handleCloseDrawer} />
-						{/* 여기에 챗 이력 목록을 추가하세요 */}
-					</div>
-				</Drawer>
-			</div>
-		</>
+		<div className="py-4 px-0 text-center relative">
+			<h2 className="mt-0 mb-0 p-0 text-xl w-full text-center">{botName}</h2>
+			<Dropdown
+				className="h-fit px-0 py-1 shadow-none hover:shadow-none text-center text-xs mr-1 pr-4"
+				value={`${currentModel.provider}/${currentModel.model}`}
+				onChange={handleChangeModel}
+				disabled={disabled}
+			>
+				{providers
+					?.filter(({provider, models}) => models.length > 0)
+					.map(({provider, models}) => {
+						return (
+							<optgroup key={provider} label={provider}>
+								{models.map(model => {
+									const value = `${provider}/${model}`;
+									return (
+										<option key={value} value={value}>
+											{model}
+										</option>
+									);
+								})}
+							</optgroup>
+						);
+					})}
+			</Dropdown>
+
+			{leftComponent && <div className="absolute top-2 left-2">{leftComponent}</div>}
+
+			{rightComponent && <div className="absolute top-2 right-2">{rightComponent}</div>}
+
+			{children}
+		</div>
 	);
 };
