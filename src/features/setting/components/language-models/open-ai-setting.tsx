c828c187d8836fa039354134f6e84364f761bb56
anpigon <markan82@gmail.com>
2024-11-01 01:01:12 +0900

    refactor(open-ai-setting.tsx): consolidate and simplify open ai setting component for better readability and maintainability fix(open-ai-setting.tsx): remove redundant state variables and event handlers to streamline the code logic

--- src/features/setting/components/language-models/open-ai-setting.tsx
+++ src/features/setting/components/language-models/open-ai-setting.tsx
@@ -1,12 +1,9 @@
 import {ChangeEventHandler, useCallback, useState} from 'react';
 import {useTranslation} from 'react-i18next';
-import {twMerge} from 'tailwind-merge';
-import clsx from 'clsx';
 
 import {SettingItem} from '@/components/settings/setting-item';
 import {usePlugin, useSettings} from '@/hooks/useApp';
 import {useSettingDispatch} from '../../context';
-import {Toggle} from '@/components/form/toggle';
 
 export const OpenAiSetting = () => {
 	const {t} = useTranslation('settings');
@@ -16,9 +13,7 @@ export const OpenAiSetting = () => {
 	const {refreshChatbotView} = useSettingDispatch();
 	const providerSettings = settings.providers.OPEN_AI;
 
-	const [enable, setEnable] = useState(providerSettings?.enable ?? false);
 	const [apiKey, setApiKey] = useState(providerSettings?.apiKey ?? '');
-	const [allowStream, setAllowStream] = useState(providerSettings?.allowStream);
 
 	const saveSettings = useCallback(async () => {
 		await plugin.saveSettings();
@@ -32,36 +27,9 @@ export const OpenAiSetting = () => {
 		void saveSettings();
 	};
 
-	const handleAllowStreamChange: ChangeEventHandler<HTMLInputElement> = event => {
-		const value = event.target.checked;
-		setAllowStream(value);
-		providerSettings.allowStream = value;
-		void saveSettings();
-	};
-
 	return (
-		<>
-			<SettingItem heading name={t('OpenAI')} className="bg-secondary rounded-lg !px-3  mt-1">
-				<Toggle
-					checked={enable}
-					onChange={event => {
-						const value = event.target.checked;
-						setEnable(value);
-						providerSettings.enable = value;
-						void saveSettings();
-					}}
-				/>
-			</SettingItem>
-
-			<div className={twMerge(clsx('p-3 hidden', {block: enable}))}>
-				<SettingItem name={t('Provider API Key', {name: 'OpenAI API'})} description={t('Insert your provider API Key', {name: 'OpenAI API'})}>
-					<input type="password" spellCheck={false} placeholder="sk-aOO-...Cvll" defaultValue={apiKey} onChange={handleApiKeyChange} />
-				</SettingItem>
-
-				<SettingItem name={t('Allow Stream')} description={t('Allow the model to stream responses.', {name: 'OpenAI'})}>
-					<Toggle name="allowStream" checked={allowStream} onChange={handleAllowStreamChange} />
-				</SettingItem>
-			</div>
-		</>
+		<SettingItem name={t('Provider API Key', {name: 'OpenAI API'})} description={t('Insert your provider API Key', {name: 'OpenAI API'})}>
+			<input type="password" spellCheck={false} placeholder="sk-aOO-...Cvll" defaultValue={apiKey} onChange={handleApiKeyChange} />
+		</SettingItem>
 	);
 };
