0c920b08e58cb151eba98a163d46ddd20131ed14
anpigon <markan82@gmail.com>
2024-10-31 21:56:10 +0900

    chore: update package.json

--- commitlint.config.mjs
+++
@@ -1,9 +0,0 @@
-export default {
-	extends: ['@commitlint/config-conventional'],
-	rules: {
-		'header-max-length': [1, 'always', 100],
-		'body-max-line-length': [1, 'always', Infinity],
-		'subject-case': [1, 'always', ['lower-case', 'upper-case', 'camel-case', 'kebab-case', 'pascal-case', 'sentence-case', 'snake-case', 'start-case']],
-		'body-leading-blank': [1, 'always'],
-	},
-};
