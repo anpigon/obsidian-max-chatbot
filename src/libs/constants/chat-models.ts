4a32a7ed72de8eaa013254d0fd730561823e844e
anpigon <markan82@gmail.com>
2024-11-01 00:19:02 +0900

    feat(chat-models.ts): add detailed model configurations for Anthropic, Google Gemini, and Sambanova providers

--- src/libs/constants/chat-models.ts
+++ src/libs/constants/chat-models.ts
@@ -28,11 +28,77 @@ export const CHAT_MODEL_OPTIONS = {
 			max_output_tokens: 65_536,
 		},
 	],
-	[LLM_PROVIDERS.ANTHROPIC]: [],
-	[LLM_PROVIDERS.GOOGLE_GEMINI]: [],
+	// https://docs.anthropic.com/en/docs/about-claude/models#model-names
+	[LLM_PROVIDERS.ANTHROPIC]: [
+		{
+			id: 'claude-3-5-sonnet-latest',
+			name: 'claude-3-5-sonnet',
+			context_window: 200_000,
+			max_output_tokens: 8_192,
+		},
+	],
+	// https://ai.google.dev/gemini-api/docs/models/gemini?hl=ko
+	[LLM_PROVIDERS.GOOGLE_GEMINI]: [
+		{
+			id: 'gemini-1.5-flash',
+			name: 'gemini-1.5-flash',
+			context_window: 1_048_576,
+			max_output_tokens: 8_192,
+		},
+		{
+			id: 'gemini-1.5-pro',
+			name: 'gemini-1.5-pro',
+			context_window: 2_097_152,
+			max_output_tokens: 8_192,
+		},
+	],
+	// https://community.sambanova.ai/docs?topic=193
+	[LLM_PROVIDERS.SAMBANOVA]: [
+		{
+			id: 'Meta-Llama-3.1-8B-Instruct',
+			name: 'Llama 3.1 8B',
+			context_window: 16_000,
+			max_output_tokens: 4_096,
+		},
+		{
+			id: 'Meta-Llama-3.1-70B-Instruct',
+			name: 'Llama 3.1 70B',
+			context_window: 64_000,
+			max_output_tokens: 4_096,
+		},
+		{
+			id: 'Meta-Llama-3.1-405B-Instruct',
+			name: 'Llama 3.1 405B',
+			context_window: 8_000,
+			max_output_tokens: 4_096,
+		},
+		{
+			id: 'Meta-Llama-3.2-1B-Instruct',
+			name: 'Llama 3.2 1B',
+			context_window: 4_096,
+			max_output_tokens: 4_096,
+		},
+		{
+			id: 'Meta-Llama-3.2-3B-Instruct',
+			name: 'Llama 3.2 3B',
+			context_window: 4_096,
+			max_output_tokens: 4_096,
+		},
+		{
+			id: 'Llama-3.2-11B-Vision-Instruct',
+			name: 'Llama 3.2 11B',
+			context_window: 4_096,
+			max_output_tokens: 4_096,
+		},
+		{
+			id: 'Llama-3.2-90B-Vision-Instruct',
+			name: 'Llama 3.2 90B',
+			context_window: 4_096,
+			max_output_tokens: 4_096,
+		},
+	],
+	[LLM_PROVIDERS.GROQ]: [],
+	[LLM_PROVIDERS.UPSTAGE]: [],
 	[LLM_PROVIDERS.MISTRAL]: [],
 	[LLM_PROVIDERS.OPEN_ROUTER]: [],
-	[LLM_PROVIDERS.UPSTAGE]: [],
-	[LLM_PROVIDERS.GROQ]: [],
-	[LLM_PROVIDERS.SAMBANOVA]: [],
 };
