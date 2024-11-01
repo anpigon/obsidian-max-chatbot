d643dd6c2b8ff6c991e7f36b64efd098b525de9e
anpigon <markan82@gmail.com>
2024-11-01 11:40:01 +0900

    refactor(index.tsx): reorder import statements for consistency and remove commented out components chore(index.tsx): update import paths and rename variables to PascalCase

--- src/features/setting/components/language-models/index.tsx
+++ src/features/setting/components/language-models/index.tsx
@@ -2,12 +2,11 @@ import {useTranslation} from 'react-i18next';
 
 import {SettingItem} from '@/components/settings/setting-item';
 
-import { AnthropicSetting } from './anthropic-setting';
-import { UpstageSetting } from './upstage-setting';
-import { GoogleSetting } from './google-setting';
+import {AnthropicSetting} from './anthropic-setting';
+import {UpstageSetting} from './upstage-setting';
 import {OpenAiSetting} from './open-ai-setting';
-import { GroqSetting } from './groq-setting';
-import { SambaBovaSetting } from './sambanova-setting';
+import {GoogleSetting} from './google-setting';
+import {GroqSetting} from './groq-setting';
 
 export default function LanguageModels() {
 	const {t} = useTranslation('settings');
@@ -17,14 +16,12 @@ export default function LanguageModels() {
 			<SettingItem heading name={t('Language models')} />
 			{/* <OllamaSetting /> */}
 			{/* <RestApiSetting /> */}
+			{/* `<SambaBovaSetting />` */}
 			<OpenAiSetting />
 			<AnthropicSetting />
 			<GoogleSetting />
 			<GroqSetting />
 			<UpstageSetting />
-			<div className="hidden">
-				<SambaBovaSetting />
-			</div>
 		</>
 	);
 }
